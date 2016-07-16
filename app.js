57c633b98f3b031530eaaf86a3d11a5eaca3dc79
rasoul poordelan <rasoulpoordelan@gmail.com>
2016-07-16 12:52:35 +0430

    changePort

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 ﻿var express = require('express');
 var app = express();
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/hello', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+app.listen(port ,function () {
+  console.log('Example app listening on port '+port);
 });
